c488a6db599304c567005b7267ca40bf42996e27
PolinaLihane <polin@list.ru>
2022-10-18 22:30:23 +0300

    не доделанный калькулятор

---
+++ Seminar/Sem_07/function.py
@@ -0,0 +1,20 @@
+import logger as l
+
+def add(P, Q):
+    l("add")
+    return P + Q
+
+
+def subtract(P, Q):
+    l("substract")
+    return P - Q
+
+
+def multiply(P, Q):
+    l("multiply")
+    return P * Q
+
+
+def divide(P, Q):
+    l("multiply")
+    return P / Q
